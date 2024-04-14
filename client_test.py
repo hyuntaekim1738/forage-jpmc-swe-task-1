fd02fa1712566ab75e5ac8d118c7a3fa991c0c8c
hyuntaekim1738 <hyuntaekim1738@gmail.com>
2024-04-13 19:29:58 -0500

    Task1Test

--- client_test.py
+++ client_test.py
@@ -7,8 +7,12 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
-
+    expectedData = [ #stock, bid_price, ask_price, price
+      ('ABC', 120.48, 121.2, 120.84),
+      ('DEF', 117.87, 121.68, 119.775)
+    ]
+    for i in range(len(quotes)):
+      self.assertEqual(getDataPoint(quotes[i]), expectedData[i])
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
